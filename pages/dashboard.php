9237dd7c7cbb6937c89b25999f7ec35db0bbb310
Hotman-ZS <siahaanhotman165@gmail.com>
2025-10-18 15:03:36 +0700

    ganti nama

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -4,7 +4,7 @@
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="index.html">Home</a></li>
           <li class="breadcrumb-item">Pages</li>
-          <li class="breadcrumb-item active">Blank</li>
+          <li class="breadcrumb-item active">Ini Dashboard</li>
         </ol>
       </nav>
     </div>
